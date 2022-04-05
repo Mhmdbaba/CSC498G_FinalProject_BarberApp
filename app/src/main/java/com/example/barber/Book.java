ad97b9d9d9d8d3844214548c675a0e08afd11f3e
Mohammad Elbaba <mhmdbaba2001@gmail.com>
2022-04-05 11:44:28 +0300

    commit 10

--- app/src/main/java/com/example/barber/Book.java
+++ app/src/main/java/com/example/barber/Book.java
@@ -5,16 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.sql.Time;
+import java.util.ArrayList;
 
 public class Book extends AppCompatActivity {
 
     Spinner sp;
+    CheckBox cb_haircut;
+    CheckBox cb_beardtrim;
+    CheckBox cb_hottowels;
+    CheckBox cb_scalpmassage;
+    CheckBox cb_antidandruff;
+
+    ArrayList<String> selected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book);
+        cb_haircut = (CheckBox) findViewById(R.id.cb_haircut);
+        cb_beardtrim = (CheckBox) findViewById(R.id.cb_beardtrim);
+        cb_hottowels = (CheckBox) findViewById(R.id.cb_hottowel);
+        cb_scalpmassage = (CheckBox) findViewById(R.id.cb_scalpmessage);
+        cb_antidandruff = (CheckBox) findViewById(R.id.cb_antidandruff);
 
         //Get the available time slots from database and add them to spinner (sp) by array adapter
 
@@ -24,7 +41,20 @@ public class Book extends AppCompatActivity {
         Button btn = (Button) view;
 
         if (btn.getTag().toString().equalsIgnoreCase("book")){
+            if (cb_haircut.isChecked())
+                selected.add(cb_haircut.getText().toString());
+            if (cb_beardtrim.isChecked())
+                selected.add(cb_beardtrim.getText().toString());
+            if (cb_hottowels.isChecked())
+                selected.add(cb_hottowels.getText().toString());
+            if (cb_scalpmassage.isChecked())
+                selected.add(cb_scalpmassage.getText().toString());
+            if (cb_antidandruff.isChecked())
+                selected.add(cb_antidandruff.getText().toString());
 
+            if (sp.getSelectedItem() == null && selected.isEmpty()) {
+                Toast.makeText(this, "Please fill the required blanks!", Toast.LENGTH_LONG);
+            }
         }
 
     }
