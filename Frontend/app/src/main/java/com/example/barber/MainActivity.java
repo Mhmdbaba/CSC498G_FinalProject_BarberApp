a8e999dfaa46cb491e2fb474083aa92666fc026f
Mohammad Elbaba <mhmdbaba2001@gmail.com>
2022-04-16 16:48:14 +0300

    commit 24

--- Frontend/app/src/main/java/com/example/barber/MainActivity.java
+++ Frontend/app/src/main/java/com/example/barber/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.barber;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,6 +12,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.reflect.MalformedParameterizedTypeException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -21,10 +24,45 @@ public class MainActivity extends AppCompatActivity {
     String input_username;
     String input_password;
 
+
+    public class DownloadTask extends AsyncTask<String, Void, String>{
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection http;
+
+            try {
+                url = new URL(urls[0]);
+                http = (HttpURLConnection) url.openConnection();
+
+                InputStream in = http.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data= reader.read();
+
+                while (data != -1){
+                    char curr = (char) data;
+                    result +=curr;
+                    data = reader.read();
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+                return null;
+            }
+            return result;
+        }
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        String url = "http://192.168.157.1/BarberServer/login.php";
+        DownloadTask task = new DownloadTask();
+        task.execute(url);
 
     }
 
